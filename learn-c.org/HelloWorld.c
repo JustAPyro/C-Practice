063dfd783009ed10c4a595b5de553d1bd13849ce
Luke Hanna <51210411+JustAPyro@users.noreply.github.com>
2022-04-19 13:26:17 -0400

    Hello World!

---
+++ learn-c.org/HelloWorld.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+// https://www.learn-c.org/en/Hello%2C_World%21
+
+// Main entry point, just a like java
+int main() 
+{
+    // Printf to display output and then return 0 to indicate success
+    printf("Hello World!");
+    return 0;
+}
